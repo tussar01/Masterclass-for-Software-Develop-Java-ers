7335d93dc9f6139780ab55c0b4f79d6fecf67e93
tussar01 <46920706+tussar01@users.noreply.github.com>
2022-10-26 09:34:57 +0300

    Create WhileRecap.java

---
+++ 5-Control Flow Statements/WhileRecap.java
@@ -0,0 +1,45 @@
+
+
+public class Main {
+  
+  // 1. We can interrupt the loop by using a "continue" and/or a "break" statement.
+ //2. With the continue keyword the loop will bypass the part of the code block that is below the continue keyworh and 
+  
+    public static void main(String[] args) {
+
+        int number = 0;
+        while (number < 15) {
+            number++;
+
+            if (number <= 5) {
+                System.out.println("Skipping number " + number);
+                continue;
+            }
+
+            System.out.println("number= " + number);
+
+            if(number >= 10) {
+                System.out.println("Breaking at " + number);
+                break;
+            }
+        }
+    }
+}
+
+
+/*
+output :
+
+Skipping number 1
+Skipping number 2
+Skipping number 3
+Skipping number 4
+Skipping number 5
+number= 6
+number= 7
+number= 8
+number= 9
+number= 10
+Breaking at 10
+
+*/
