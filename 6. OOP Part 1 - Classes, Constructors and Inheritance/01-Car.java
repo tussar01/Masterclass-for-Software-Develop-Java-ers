312dd76f7703fa22bbe3d694000f643d2b7b6391
tussar01 <46920706+tussar01@users.noreply.github.com>
2022-10-31 19:01:05 +0300

    Update 01-Car.java

--- 6. OOP Part 1 - Classes, Constructors and Inheritance/01-Car.java
+++ 6. OOP Part 1 - Classes, Constructors and Inheritance/01-Car.java
@@ -1,1 +1,33 @@
 
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("commodore")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.setModel("carrera");
+        System.out.println("Model is " + porsche.getModel());
+    }
+}
